f9d39781944d1b7a4b37bb239e2c7d7837cc096d
Doophie <128660812+oogeuehh@users.noreply.github.com>
2024-08-10 19:07:22 +0800

    Update dictionarySignature.py

--- dictionarySignature.py
+++ dictionarySignature.py
@@ -1,18 +1,7 @@
+import argparse
 import hashlib
 import base64
 from hashlib import sha256
-
-def calculate_secretkey(seed):
-    print("Seed: ", seed)
-    hashed_seed = (sha256(seed.encode('utf-8')).hexdigest())
-    print("SHA256: ", hashed_seed)
-    hex_seed = bytes.fromhex(hashed_seed)
-    print("")
-    print("From HEX: ", hex_seed)
-    base64_bytes = base64.b64encode(hex_seed)
-    base64_seed = base64_bytes.decode('ascii')
-    print("To base64 (KEY): ", base64_seed)
-    print("Signature = sha256_48(secret_key + header + payload + CRC + Link ID + timestamp)")
 
 def calculate_secretkey_input(hashed_seed, header, payload, crc, linkid, timestamp):
     signaturecompleta = hashed_seed + header + payload + crc + linkid + timestamp
@@ -24,11 +13,11 @@ def calculate_secretkey_input(hashed_seed, header, payload, crc, linkid, timesta
 def crack_hash(hash_type, hash_string, header, payload, crc, linkid, timestamp, wordlist, verbose=False):
     supported_types = ('md5', 'sha256', 'sha1', 'sha224', 'sha384')
     if hash_type not in supported_types:
-        print(f"hash type '{hash_type}' is not supported.")
+        print(f"Hash type '{hash_type}' is not supported.")
         print("Supported types:")
         for hashtype in supported_types:
             print("  " + hashtype)
-        return "Unsupported hash type"
+        return
     
     cracked = []
     with open(wordlist, 'r') as wl:
@@ -37,15 +26,33 @@ def crack_hash(hash_type, hash_string, header, payload, crc, linkid, timestamp, 
             hashed_seed = (sha256(guess.encode('utf-8')).hexdigest())
             hashed_guess = calculate_secretkey_input(hashed_seed, header, payload, crc, linkid, timestamp)
             if hashed_guess == hash_string:
-                result = f"FOUND:\n{hash_string}: {guess}"
-                print(result)
-                cracked.append(result)
+                print("\nFOUND:")
+                print(f"{hash_string}: {guess}")
+                cracked.append(f"{hash_string}: {guess}")
                 break
             elif verbose:
                 print(f"Fail \"{guess}\" ({guesses.index(guess) + 1}/{len(guesses)})")
         print("End of the list.")
     
     if cracked:
-        return "\nRESULT:\n" + "\n".join(cracked)
-    else:
-        return "No match found"
+        print("\nRESULT:")
+        for crackedhash in cracked:
+            print(crackedhash)
+
+def main():
+    parser = argparse.ArgumentParser(description='SHRACK: The hash cracker')
+    parser.add_argument('--type', help='Hash type', required=True)
+    parser.add_argument('--string', help='Hash string', required=True)
+    parser.add_argument('--wordlist', help='Wordlist', required=True)
+    parser.add_argument('--header', help='Header', required=True)
+    parser.add_argument('--payload', help='Payload', required=True)
+    parser.add_argument('--crc', help='CRC', required=True)
+    parser.add_argument('--linkid', help='Link ID', required=True)
+    parser.add_argument('--timestamp', help='Timestamp', required=True)
+    parser.add_argument('--v', help="(true/false) Show more information while cracking", default=False, type=lambda x: (str(x).lower() == 'true'))
+    args = parser.parse_args()
+
+    crack_hash(args.type, args.string, args.header, args.payload, args.crc, args.linkid, args.timestamp, args.wordlist, args.v)
+
+if __name__ == "__main__":
+    main()
