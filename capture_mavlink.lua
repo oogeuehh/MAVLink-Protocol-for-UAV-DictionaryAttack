94bc5b273a19b141c5000ad3d9ca90d2d8ed50a4
Doophie <128660812+oogeuehh@users.noreply.github.com>
2024-08-13 13:24:34 +0800

    Add files via upload

---
+++ capture_mavlink.lua
@@ -0,0 +1,22 @@
+local tap = Listener.new("frame", "udp.port == 14550")
+local captured = false
+local file = io.open("/home/doophie/MAVLink-Protocol-for-UAV-DictionaryAttack/mavlink_hex_stream.txt", "w")
+
+function tap.packet(pinfo, tvb)
+	if not captured then
+		local mavlink_data = tvb:range(42):tvb()
+		local hex_stream = mavlink_data:bytes():tohex(false, " ")
+		
+		local msgid = tvb:range(49, 3):le_uint()
+		
+		if msgid == 0 then
+			file:write(hex_stream .. "\n")
+			captured = true
+			file:close()
+		end
+	end
+end
+
+function tap.reset()
+	file:close()
+end
