f2401ad5969f02ad07799748e0399039d917c0a5
Doophie <128660812+oogeuehh@users.noreply.github.com>
2024-09-04 20:03:16 +0800

    Add files via upload

---
+++ crcCheck.py
@@ -0,0 +1,39 @@
+header_hex = input("enter the header: ")
+payload_hex =input("enter the payload: ")
+
+header = bytes.fromhex(header_hex)[1:]
+payload = bytes.fromhex(payload_hex)
+message=header+payload
+print(header)
+print(payload)
+
+def crc_accumulate(byte, crc):
+    tmp = byte ^ (crc & 0xff)
+    tmp ^= (tmp << 4) & 0xff
+    return ((crc >> 8) & 0xff) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)
+
+def crc_calculate(bytes, length):
+    crc = 0xffff
+    for i in range(length):
+        crc = crc_accumulate(bytes[i], crc)
+    return crc
+
+crc = crc_calculate(message, len(message))
+
+# CRC_EXTRA value table
+crc_extra_list = [50, 124, 137, 0, 237, 217, 104, 119, 0, 0, 0, 89, 0, 0, 0, 0, 0, 0, 0, 0, 214, 159, 220, 168, 24, 23, 170, 144,
+67, 115, 39, 246, 185, 104, 237, 244, 222, 212, 9, 254, 230, 28, 28, 132, 221, 232, 11, 153, 41, 39, 214, 223,
+141, 33, 15, 3, 100, 24, 239, 238, 30, 200, 183, 0, 130, 0, 148, 21, 0, 52, 124, 0, 0, 0, 20, 0, 152, 143, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 231, 183, 63, 54, 0, 0, 0, 0, 0, 0, 0, 175, 102, 158, 208, 56, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 204, 49, 170, 44, 83, 46, 0]
+
+crc_extra = crc_extra_list["please input the tested message id as a int"]
+
+# CRC_EXTRA --> final CRC
+crc = crc_accumulate(crc_extra, crc)
+
+crc_hex = crc.to_bytes(2, 'little').hex()
+print(crc_hex)
---
+++ pymavlink_back.py
@@ -0,0 +1,97 @@
+from pymavlink import mavutil
+import time
+import hashlib
+import struct
+
+def crc_accumulate(byte, crc):
+    tmp = byte ^ (crc & 0xff)
+    tmp ^= (tmp << 4) & 0xff
+    return ((crc >> 8) & 0xff) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)
+
+def crc_calculate(bytes, length):
+    crc = 0xffff
+    for i in range(length):
+        crc = crc_accumulate(bytes[i], crc)
+    return crc
+
+# 用户输入
+seed = input("Enter seed: ")
+secreteKey = hashlib.sha256(seed.encode('utf-8')).hexdigest()
+linkid = input("Enter linkid (in hex format): ")
+
+def build_message():
+    target_system = 1
+    target_component = 1
+    # 创建 MAVLink 消息
+    command = mavutil.mavlink.MAVLink_command_long_message(
+        target_system=target_system,
+        target_component=target_component,
+        command=mavutil.mavlink.MAV_CMD_DO_SET_MODE,
+        confirmation=0,
+        param1=1.0, param2=6.0, param3=0, 
+        param4=0, param5=0, param6=0, param7=0
+    )
+    return command.pack(master.mav)[6:-2]
+
+# 构建消息
+#command_message = build_message()
+
+# 打包消息，获取完整的消息（包含头部和 payload）
+# packed_message = command_message.pack(mavutil.mavlink.MAVLink('', 1))
+#packed_message = command_message.pack()
+#payload = packed_message[6:-2]  # only payload
+
+master = mavutil.mavlink_connection('udpout:10.0.2.5:48307')
+payload = build_message()
+
+# MAVLink 2 头信息
+message_id = command_message.get_msgId()
+payload_length = len(payload)
+
+# 创建 MAVLink 2 消息头
+header = bytearray()
+header.extend(bytearray([0xFD]))  # STX (Start of Text)
+header.extend(payload_length.to_bytes(1, 'little'))  # Payload length
+header.extend(bytearray([1])) # inc
+header.extend(bytearray([0])) # cmp
+header.extend(bytearray([0]))  # Sequence number
+header.extend(bytearray([255])) 
+header.extend(bytearray([190])) 
+header.extend(message_id.to_bytes(3, 'little')) 
+
+# 计算 CRC
+message_with_header = header + payload
+crc = crc_calculate(message_with_header, len(message_with_header))
+
+# 获取 CRC_EXTRA 值
+crc_extra_list = [50, 124, 137, 0, 237, 217, 104, 119, 0, 0, 0, 89, 0, 0, 0, 0, 0, 0, 0, 0, 214, 159, 220, 168, 24, 23, 170, 144, 67, 115, 39, 246, 185, 104, 237, 244, 222, 212, 9, 254, 230, 28, 28, 132, 221, 232, 11, 153, 41, 39, 214, 223, 141, 33, 15, 3, 100, 24, 239, 238, 30, 200, 183, 0, 130, 0, 148, 21, 0, 52, 124, 0, 0, 0, 20, 0, 152, 143, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 231, 183, 63, 54, 0, 0, 0, 0, 0, 0, 0, 175, 102, 158, 208, 56, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 204, 49, 170, 44, 83, 46, 0]
+crc_extra = crc_extra_list[message_id]
+
+# CRC_EXTRA --> final CRC
+crc = crc_accumulate(crc_extra, crc)
+
+
+crc_hex = crc.to_bytes(2, 'little').hex()
+
+
+currentTime = int(time.time() * 1e6)
+timestamp = currentTime.to_bytes(8, 'little')[-6:]  # 截取低 6 字节
+timestamp_hex = timestamp.hex()
+
+header_hex = header.hex()
+payload_hex = payload.hex()
+
+signature_input_hex = secreteKey + header_hex + payload_hex + crc_hex + linkid + timestamp_hex
+
+signature_input_bytes = bytes.fromhex(signature_input_hex)
+
+hash_result = hashlib.sha256(signature_input_bytes).digest()
+signature = hash_result[:6]
+
+signed_msg = header + payload + bytes.fromhex(crc_hex) + bytes.fromhex(linkid) + timestamp + signature
+
+try:
+    master.write(signed_msg)
+    print("message sent")
+except Exception as e:
+    print("error:", str(e))
