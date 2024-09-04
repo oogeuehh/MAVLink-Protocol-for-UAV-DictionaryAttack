d463374a35c92584979035689b271344bc1a8f96
Doophie <128660812+oogeuehh@users.noreply.github.com>
2024-09-04 19:44:27 +0800

    Update timestampCheck.py

--- timestampCheck.py
+++ timestampCheck.py
@@ -1,17 +1,18 @@
 from datetime import datetime
+import binascii
 
-# 输入的时间戳
-timestamp_str = '2024-09-03 16:43:00.465050000'
+# 定义时间戳
+timestamp_str = "2024-09-03 16:43:46.505000"
+timestamp_format = "%Y-%m-%d %H:%M:%S.%f"
 
-# 将时间戳字符串转换为 datetime 对象
-dt = datetime.strptime(timestamp_str, '%Y-%m-%d %H:%M:%S.%f')
+# 将时间戳转换为 datetime 对象
+dt = datetime.strptime(timestamp_str, timestamp_format)
 
-# 计算自 Unix 纪元以来的毫秒数
-unix_epoch = datetime(1970, 1, 1)
-milliseconds = int((dt - unix_epoch).total_seconds() * 1000 + dt.microsecond / 1000)
+# 转换为 Unix 时间戳（秒）
+unix_timestamp = int(dt.timestamp())
 
-# 将毫秒数转换为十六进制字符串
-hex_stream = format(milliseconds, 'x')
+# 转换为十六进制
+hex_stream = binascii.hexlify(unix_timestamp.to_bytes((unix_timestamp.bit_length() + 7) // 8, byteorder='big')).decode()
 
-# 打印结果
+# 输出结果
 print(hex_stream)
