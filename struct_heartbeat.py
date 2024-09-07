288c142f1d927cb35b645d474a5e27f378d34d2c
Doophie <128660812+oogeuehh@users.noreply.github.com>
2024-09-07 14:46:58 +0800

    Add files via upload

---
+++ struct_heartbeat.py
@@ -0,0 +1,94 @@
+from pymavlink import mavutil
+import time
+import hashlib
+import struct
+
+def crc_accumulate(byte, crc):
+    tmp = byte ^ (crc & 0xff)
+    tmp ^= (tmp << 4) & 0xff
+    return ((crc >> 8) & 0xff) ^ (tmp << 8) ^ (tmp << 3) ^ (tmp >> 4)
+
+def crc_calculate(bytes, length):
+    crc = 0xffff
+    for i in range(length):
+        crc = crc_accumulate(bytes[i], crc)
+    return crc
+
+# 用户输入
+seed = input("Enter seed: ")
+secreteKey = hashlib.sha256(seed.encode('utf-8')).hexdigest()
+linkid = input("Enter linkid (in hex format): ")
+
+
+# heartbeat payload
+TYPE = 6
+AUTOPILOT = 8
+BASE_MODE = 0
+CUSTOM_MODE = 0
+SYSTEM_STATUS = 0
+MAVLink_version = 3
+
+PAYLOAD = struct.pack('<BBB4BBB', TYPE, AUTOPILOT, BASE_MODE, CUSTOM_MODE, SYSTEM_STATUS, MAVLink_version)
+
+
+# header
+STX = 0XFD
+LEN = len(PAYLOAD)
+INC = 1
+CMP = 0
+SEQ = 255
+SYSID = 255
+COMPID = 190
+MSGID = [111, 111, 111]
+
+HEADER = struct.pack('<BBBBBBB3s{}S'.format(LEN), STX, LEN, INC, CMP, SEQ, SYSID, COMPID, MSGID, PAYLOAD)
+
+
+# SIGNATURE
+# CRC_EXTRA value table
+crc_extra_list = [50, 124, 137, 0, 237, 217, 104, 119, 0, 0, 0, 89, 0, 0, 0, 0, 0, 0, 0, 0, 214, 159, 220, 168, 24, 23, 170, 144,
+67, 115, 39, 246, 185, 104, 237, 244, 222, 212, 9, 254, 230, 28, 28, 132, 221, 232, 11, 153, 41, 39, 214, 223,
+141, 33, 15, 3, 100, 24, 239, 238, 30, 200, 183, 0, 130, 0, 148, 21, 0, 52, 124, 0, 0, 0, 20, 0, 152, 143, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 231, 183, 63, 54, 0, 0, 0, 0, 0, 0, 0, 175, 102, 158, 208, 56, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 204, 49, 170, 44, 83, 46, 0]
+
+message_with_header = header[1:] + payload # header not include STX
+crc = crc_calculate(message_with_header, len(message_with_header))
+crc_extra = crc_extra_list[message_id]
+
+# CRC_EXTRA --> final CRC
+crc = crc_accumulate(crc_extra, crc)
+
+crc_hex = crc.to_bytes(2, 'little').hex()
+
+currentTime = int(time.time() * 1e6)
+timestamp = currentTime.to_bytes(8, 'little')[-6:]  # 截取低 6 字节
+timestamp_hex = timestamp.hex()
+
+header_hex = header.hex()
+payload_hex = payload.hex()
+
+# 生成签名输入的十六进制字符串形式
+signature_input_hex = secreteKey + header_hex + payload_hex + crc_hex + linkid + timestamp_hex
+
+# 将十六进制字符串转换为字节
+signature_input_bytes = bytes.fromhex(signature_input_hex)
+
+# 生成签名
+hash_result = hashlib.sha256(signature_input_bytes).digest()
+signature = hash_result[:6]
+
+# 组合最终的消息
+signed_msg = header + payload + bytes.fromhex(crc_hex) + bytes.fromhex(linkid) + timestamp + signature
+
+# send message
+try:
+    master = mavutil.mavlink_connection('udpout:10.0.2.5:14551')
+    master.wait_heartbeat()
+    master.write(signed_msg)
+    print("message sent")
+except Exception as e:
+    print("error:", str(e))
